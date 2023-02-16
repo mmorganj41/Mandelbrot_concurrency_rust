9eb08b2ff2119cc4b75dc3a16e8088dc868024f0
mmorganj41 <mmorganj41@gmail.com>
2023-02-16 13:22:53 -0500

    write iamge from pixels and bounds function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use image::ImageFormat::PNG;
 use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
 
 fn main() {
     println!("Hello, world!");
@@ -118,4 +122,18 @@ fn render(pixels: &mut [u8],
                 }
         }
     }
+}
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
+/// file named `filename`,
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+    -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels,
+                   bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+    Ok(())
 }

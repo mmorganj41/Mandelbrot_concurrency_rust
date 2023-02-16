2264d56921c2d66c4ec73b9d8b5201f4ce8a2b18
mmorganj41 <mmorganj41@gmail.com>
2023-02-16 12:42:18 -0500

    change square to have a limit

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn complex_square_add_loop(c: Complex<f64>) {
+/// Try to determine if 'c' is in the mandelbrot set, using at most `limit`
+/// iterations to decide
+/// 
+/// If `c` is not a member, return `Some(i), where `i` is the number of
+/// iterations it took for `c` to leave the circle of radius 2 centered on the origin
+/// If `c` seems to be a emmber (more precisely, if we reached the iteration
+/// limit without being able to prove that `c` is not a member), return
+/// `None`.
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
         z = z * z + c;
     }
+
+    None
 }

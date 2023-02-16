e03791effb0e021d92c15173d6ecb4b83eb3324c
mmorganj41 <mmorganj41@gmail.com>
2023-02-16 13:10:10 -0500

    create function to convert pixel to point in plane

--- src/main.rs
+++ src/main.rs
@@ -72,3 +72,24 @@ fn test_parse_complex() {
                Some(Complex { re: 1.25, im: -0.625}));
     assert_eq!(parse_complex(",-0.0625"), None);
 }
+
+/// Given the row and column of a pixel in the ouput image, return the
+/// corresponding point on the complex plane.
+/// 
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+/// The `upper_left` and ` lower_right` parameters are points on the complex 
+/// plane designating the area our image covers
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                                     upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}

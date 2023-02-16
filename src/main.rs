df4a354558b21097477cca852f10dc4181ceef57
mmorganj41 <mmorganj41@gmail.com>
2023-02-16 13:30:28 -0500

    make main fn command line args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::ImageFormat::PNG;
+use std::env;
 use num::Complex;
 use std::str::FromStr;
 use image::ColorType;
@@ -6,7 +6,29 @@ use image::png::PNGEncoder;
 use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env:: args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", 
+                args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
 /// Try to determine if 'c' is in the mandelbrot set, using at most `limit`
